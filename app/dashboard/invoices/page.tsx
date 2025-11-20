e9b83bd16b47d4f550abe6cd0759e64e7728353f
yanliang-code <1205836625@qq.com>
2025-11-20 20:49:16 +0800

    服务端动态获取接口，渲染组件 page页面增加骨架图加载 优化 page 中部分接口导致整体加载慢 --- 分解到组件中

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
+export default async function Page() {
+  await new Promise((resolve, reject) => {
+    setTimeout(resolve, 4000);
+  });
   return <p>Invoices Page</p>;
 }
