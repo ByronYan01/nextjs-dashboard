864775922f9ee6773e32a97104ca85a52e1a8fef
yanliang-code <1205836625@qq.com>
2025-11-21 20:59:26 +0800

    增加元数据，利于seo

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,15 @@ import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from "next";
+
+// 重复的通过 layout.tsx 中的 metadata.title.template 定义
+// export const metadata: Metadata = {
+//   title: "Invoices | Acme Dashboard",
+// };
+export const metadata: Metadata = {
+  title: "Invoices",
+};
 
 export default async function Page(props: {
   searchParams?: Promise<{
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+// nextjs 会自动在 head 中添加元数据
+export const metadata: Metadata = {
+  title: {
+    // 定义模版，%s 会自动替换为当前页面的 title
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 /**
  * 根布局
